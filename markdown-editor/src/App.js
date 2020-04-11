bf3f5ea90ff3436d33b5ff65717d96bde9b0a491
Carlos Del Real <carloshdelreal@gmail.com>
2020-04-11 13:24:58 -0500

    Markdown editor by Carlos del Real

---
+++ markdown-editor/src/App.js
@@ -0,0 +1,39 @@
+import React, { useState }from 'react';
+import {UnControlled as CodeMirror} from 'react-codemirror2'
+import MarkdownView from 'react-showdown';
+
+const App = () => {
+  const [mdContent, setValue] = useState("# Type your markdown");
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Markdown Editor</h1>
+        <p>by Carlos Del Real</p>
+      </header>
+      <div className="container">
+        <div className="row">
+
+          <div className="col-6">
+            <CodeMirror
+              value={mdContent}
+              options={{
+                mode: 'xml',
+                theme: "default",
+                lineNumbers: true
+              }}
+              onChange={(editor, data, mdContent) => {setValue(mdContent)}}
+            />
+          </div>
+
+          <div className="col-6">
+            <MarkdownView markdown={mdContent} />
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
